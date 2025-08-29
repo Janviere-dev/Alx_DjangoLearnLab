aa9f1c7c5a440876b5ce3812339851d608dec59a
Munezero Janviere <j.munezero@alustudent.com>
2025-08-29 14:49:44 +0200

    added comments and post viewsets with permission based

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+filter_backends = [DjangoFilterBackend]
+filterset_fields = ['title', 'content']
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners to edit or delete their own content.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions only for the object's author
+        return obj.author == request.user
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()  
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+

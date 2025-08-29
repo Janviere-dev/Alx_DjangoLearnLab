164862788a6b8d24bcdbfc1fc6f4f61b7a541c37
Munezero Janviere <j.munezero@alustudent.com>
2025-08-29 17:19:02 +0200

    add like/unlike post URL patterns for checker compliance

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -4,6 +4,7 @@ from .views import PostViewSet, CommentViewSet
 from django.urls import path
 from . import views
 from .views import FeedView
+from .views import LikePostView, UnlikePostView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -13,5 +14,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('some-endpoint/', views.some_view, name='some_name'),
     path('feed/', FeedView.as_view(), name='user_feed'),
+    path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', LikePostView.as_view(), name='unlike-post'),
 
 ]
